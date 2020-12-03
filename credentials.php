39a11710c7efed3fbff33ef866b5d3dd9a1c29f1
diegofalconc <72233504+diegofalconc@users.noreply.github.com>
2020-12-02 17:10:55 -0800

    Homework

---
+++ credentials.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_NAME', 'it261');
+define('DB_USER', 'diegofalconc');
+define('DB_PASSWORD', 'Panchovilla12345!');
+define('DB_HOST', 'mysql.diegofalconcostilla.com');
+
+?>
---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+<title>people page</title>
+<link href="./css/styles.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+<div id="wrapper">
+
